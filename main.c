371cd5f26bf381b1fda4969b6315b43e7be38ca0
Leonardo Freire <freirer.leonardo@gmail.com>
2020-11-05 20:15:32 -0300

    First Commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+int main (void)
+{
+    printf("  ______     _______     _           _______ \n");
+    printf(" |  __   %c  (  ____ %c   ( )         (  ____ %c \n",92,92,92);
+    printf(" | (  %c  )  | (    %c/   | |         | (    %c/ \n",92,92,92);
+    printf(" | |   ) |  | |         | |         | |      \n");
+    printf(" | |   | |  | |         | |         | |      \n");
+    printf(" | |   ) |  | |         | |         | |      \n");
+    printf(" | (__/  )_ | (____/%c _ | (____/%c _ | (____/%c \n",92,92,92);
+    printf(" (______/(_)(_______/(_)(_______/(_)(_______/\n\n");
+    printf("    Dispositivo de Calculo em Linguagem C");
+
+}
+
